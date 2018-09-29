3648ec87fc75ff656bb9b6a3130bd54e14d2a9f3
kerol <kerol@kepiture.com>
2018-09-29 14:14:20 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weixin-api",
-    version="1.3.1",
+    version="1.3.2",
     author="kerol",
     author_email="ikerol@163.com",
     description="weixin api for miniprogram and minigame",
