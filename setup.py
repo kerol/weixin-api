adb6b051c8637dc1371d08285f2122bd75cdaebf
kerol <kerol@kepiture.com>
2019-01-24 17:36:44 +0800

    fix getticket type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weixin-api",
-    version="1.3.9",
+    version="1.4.0",
     author="kerol",
     author_email="ikerol@163.com",
     description="weixin api for miniprogram and minigame",
--- weixin_api/ticket.py
+++ weixin_api/ticket.py
@@ -6,9 +6,9 @@ from weixin_api.request import requests_get
 # --------------------------------------------------------------------------- #
 
 
-def getticket(access_token, params=None, **kwargs):
+def getticket(access_token, ticket_type, params=None, **kwargs):
     """ 获取api_ticket """
-    api = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token={}&type=wx_card'.format(access_token)
+    api = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token={}&type={}'.format(access_token, ticket_type)
     params = {} if not params else params.update(**kwargs)
 
     return requests_get(api, params)
